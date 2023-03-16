68928daec9836bce8c1f5ad7c1e70fe111cf6abc
哈啦少本少 <3349098957@qq.com>
2023-03-16 08:39:40 +0800

    提交

---
+++ gitstudy/src/main/java/test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: 哈拉少本少
+ * @Date: 2023/03/16/8:28
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
